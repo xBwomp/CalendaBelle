f3e367d972236a7e5f4b013f55d1cb3ad40d6d67
Jeff <821659+xBwomp@users.noreply.github.com>
2025-08-24 12:24:51 -0400

    fix fvite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
@@ -22,14 +23,5 @@ export default defineConfig({
   build: {
     outDir: '../../dist/client',
     emptyOutDir: true
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
   }
 })
